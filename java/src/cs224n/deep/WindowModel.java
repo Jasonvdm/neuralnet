cb16a58e3496b4e98c7b88bebe0a0343da7a7970
Your Name <you@example.com>
2014-12-04 20:34:43 -0800

    synching

--- java/src/cs224n/deep/WindowModel.java
+++ java/src/cs224n/deep/WindowModel.java
@@ -11,26 +11,42 @@ import java.text.*;
 
 public class WindowModel {
 
-	protected SimpleMatrix L, W, Wout, U;
+	protected SimpleMatrix L, W, Wout, U, FeatureMat;
 
 	private HashMap<String,String> exactMatches = new HashMap<String, String>();
 	//
-	public int windowSize,wordSize, hiddenSize;
+	public int windowSize,wordSize, hiddenSize, H;
+	public int K = 5;
+
 	public int[][] matrix;
 
 	public WindowModel(int _windowSize, int _hiddenSize, double _lr){
 		//TODO
+		windowSize = _windowSize;
+		wordSize = 50;
+		hiddenSize = _hiddenSize;
+		H = _hiddenSize;
 	}
 
 	/**
 	 * Initializes the weights randomly. 
 	 */
-	public void initWeights(){
+	public void initWeights(SimpleMatrix wordMat){
 		//TODO
 		// initialize with bias inside as the last column
 		// W = SimpleMatrix...
 		// U for the score
 		// U = SimpleMatrix...
+		FeatureMat = wordMat;
+		int lastIndex = wordSize*windowSize;
+		W = SimpleMatrix.random(H,lastIndex + 1,0,1, new Random());
+		for(int i = 0; i < H; i++){
+			W.set(i, lastIndex, 1);
+		}
+		U = SimpleMatrix.random(K,H+1,0,1, new Random());
+		for(int i = 0; i < K; i++){
+			U.set(i,H,1);
+		}
 	}
 
 
@@ -72,7 +88,9 @@ public class WindowModel {
 	}
 
 	private SimpleMatrix zFunction(SimpleMatrix inputVector){
-		return W.mult(inputVector);
+		SimpleMatrix newVec = SimpleMatrix.random(inputVector.getRows(),1,1,1, new Random());
+		newVec.insertIntoThis(0,0,inputVector);
+		return W.mult(newVector);
 	}
 
 	private SimpleMatrix hFunction(SimpleMatrix inputVector){
@@ -86,7 +104,10 @@ public class WindowModel {
 	}
 
 	private SimpleMatrix gFunction(SimpleMatrix inputVector){
-		SimpleMatrix gMat = U.mult(inputVector);
+		SimpleMatrix newVec = SimpleMatrix.random(inputVector.getRows(),1,1,1, new Random());
+		newVec.insertIntoThis(0,0,inputVector);
+
+		SimpleMatrix gMat = U.mult(newVec);
 		int numRows = gMat.numRows();
 		int numCols = gMat.numCols();
 		int denom = 0;
